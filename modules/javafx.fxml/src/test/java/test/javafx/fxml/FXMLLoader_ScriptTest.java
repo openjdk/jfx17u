24bd251a708dafd26ce173250615bcf77760ae7e
Ajit Ghaisas <aghaisas@openjdk.org>
2022-11-07 09:40:57 +0000

    8294779: Improve FX pages
    Reviewed-by: kcr
    Backport-of: 4f262518f48c984e72d06fce528ce2fca701664f

--- modules/javafx.fxml/src/test/java/test/javafx/fxml/FXMLLoader_ScriptTest.java
+++ modules/javafx.fxml/src/test/java/test/javafx/fxml/FXMLLoader_ScriptTest.java
@@ -26,6 +26,7 @@ package test.javafx.fxml;
 
 import com.sun.javafx.fxml.FXMLLoaderHelper;
 import org.junit.Test;
+import org.junit.Ignore;
 
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -37,10 +38,15 @@ import javax.script.ScriptEngine;
 import static org.junit.Assert.*;
 import static org.junit.Assume.assumeTrue;
 
+@Ignore
 public class FXMLLoader_ScriptTest {
     @Test
     @SuppressWarnings("deprecation")
     public void testStaticScriptLoad() throws IOException {
+        // This test needs Nashorn script engine.
+        // Test will be rewritten under - JDK-8245568
+        assumeTrue(isNashornEngineAvailable());
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("static_script_load.fxml"));
         FXMLLoaderHelper.setStaticLoad(fxmlLoader, true);
         AtomicBoolean scriptCalled = new AtomicBoolean();
