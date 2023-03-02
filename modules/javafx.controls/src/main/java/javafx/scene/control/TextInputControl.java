b3b2eeb2e44ffdef00512ea35b8e9df51820c8ee
Jose Pereda <jpereda@openjdk.org>
2023-03-02 15:51:13 +0000

    8303026: [TextField] IOOBE on setting text with control characters that replaces existing text
    Reviewed-by: angorya, arapte

--- modules/javafx.controls/src/main/java/javafx/scene/control/TextInputControl.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/TextInputControl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2011, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -182,6 +182,13 @@ public abstract class TextInputControl extends Control {
             } else {
                 int start = sel.getStart();
                 int end = sel.getEnd();
+                int length = txt.length();
+                if (end > start + length) {
+                    end = length;
+                }
+                if (start > length - 1) {
+                    start = end = 0;
+                }
                 selectedText.set(txt.substring(start, end));
             }
         }
