6f5375a8d2f97fd5300e3f2eb39b48e34a12bdc1
Florian Kirmaier <fkirmaier@openjdk.org>
2023-03-16 18:34:33 +0000

    8303680: Virtual Flow freezes after calling scrollTo and scrollPixels in succession
    Reviewed-by: angorya, jvos

--- modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
+++ modules/javafx.controls/src/main/java/javafx/scene/control/skin/VirtualFlow.java
@@ -1713,7 +1713,6 @@ public class VirtualFlow<T extends IndexedCell> extends Region {
 
         // Finally, update the scroll bars
         updateScrollBarsAndCells(false);
-        lastPosition = getPosition();
 
         // notify
         return answer;
